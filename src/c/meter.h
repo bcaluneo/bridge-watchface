bbb4e5d0734af62697f99441e115bfcc5e3b2162
Brendan Caluneo <caluneo.brendan@gmail.com>
2025-03-20 18:02:08 -0400

    Slightly increase meter sizes.

--- src/c/meter.h
+++ src/c/meter.h
@@ -3,8 +3,8 @@
 #ifndef METER_H
 #define METER_H
 
-#define BATTERY_METER_SIZE 50
-#define STEP_METER_SIZE 30
+#define BATTERY_METER_SIZE 55
+#define STEP_METER_SIZE 35
 #define METER_WIDTH 5
 
 void redraw_meters(GContext *context);
